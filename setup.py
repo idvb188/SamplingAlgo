3dcc94da7b395bb1efb6ff06f8f737ad36a87001
idvb188 <150317081+idvb188@users.noreply.github.com>
2024-03-07 17:28:41 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='SamplingAlgo',
-    version='0.1.0',
+    version='0.1.1',
     author='Ilyad',
     author_email='idvb188@gmail.com',
     description='A package for sampling algorithms including Metropolis-Hastings',
